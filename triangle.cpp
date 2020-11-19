934cfbf6a35c14ebc08cd89003e8171873ea7e34
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-11-18 20:44:26 -0500

    Load the vertex and fragment shader blobs.

--- triangle.cpp
+++ triangle.cpp
@@ -11,6 +11,7 @@
 #include <optional>
 #include <cassert>
 #include <set>
+#include <fstream>
 
 using std::cout;
 using std::cerr;
@@ -52,6 +53,23 @@ void DestroyDebugUtilsMessengerEXT(
 
     if (func)
         func(instance, debugMessenger, pAllocator);
+}
+
+static std::vector<char> readFile(const std::string& filename)
+{
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file)
+        throw std::runtime_error("failed to open file!");
+
+    size_t fileSize = static_cast<size_t>(file.tellg());
+    std::vector<char> buffer(fileSize);
+
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+
+    file.close();
+    return buffer;
 }
 
 class HelloTriangleApplication
@@ -178,6 +196,7 @@ private:
         createLogicalDevice();
         createSwapChain();
         createImageViews();
+        createGraphicsPipeline();
     }
 
     void createSurface()
@@ -475,6 +494,15 @@ private:
             if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS)
                 throw std::runtime_error("failed to create image views!");
         }
+    }
+
+    void createGraphicsPipeline()
+    {
+        auto vertShaderCode = readFile("shaders/vert.spv");
+        auto fragShaderCode = readFile("shaders/frag.spv");
+
+        cout << "vertex shader size: " << vertShaderCode.size() << endl;
+        cout << "fragment shader size: " << fragShaderCode.size() << endl;
     }
 
     QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device)
