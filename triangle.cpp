b8d80b2e5f3ebba6546d3d822b472db1f9406ed9
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-12-14 22:05:09 -0500

    Deduplicate the vertices.

--- triangle.cpp
+++ triangle.cpp
@@ -8,12 +8,16 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
 
+#include <unordered_map>
 #include <chrono>
 #include <iostream>
 #include <stdexcept>
@@ -97,7 +101,31 @@ struct Vertex
 
         return attributeDescriptions;
     }
+
+    bool operator== (const Vertex& other) const
+    {
+        return pos == other.pos && color == other.color && texCoord == other.texCoord;
+    }
+
+    bool operator!= (const Vertex& other) const
+    {
+        return !operator==(other);
+    }
 };
+
+namespace std {
+
+template<> struct hash<Vertex>
+{
+    size_t operator()(Vertex const& vertex) const
+    {
+        return ((hash<glm::vec3>()(vertex.pos) ^
+                (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^
+               (hash<glm::vec2>()(vertex.texCoord) << 1);
+    }
+};
+
+}
 
 #if 0
 const std::vector<Vertex> vertices = {
@@ -524,6 +552,8 @@ private:
         if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, MODEL_PATH.data()))
             throw std::runtime_error(warn + err);
 
+        std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+
         for (const auto& shape : shapes)
         {
             for (const auto& index : shape.mesh.indices)
@@ -543,10 +573,14 @@ private:
 
                 vertex.color = { 1.0f, 1.0f, 1.0f };
 
-                vertices.push_back(vertex);
-                indices.push_back(indices.size());
-            }
+                if (uniqueVertices.count(vertex) == 0)
+                {
+                    uniqueVertices[vertex] = vertices.size();
+                    vertices.push_back(vertex);
+                }
 
+                indices.push_back(uniqueVertices[vertex]);
+            }
         }
     }
 
